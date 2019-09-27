15e787f9aa471e949d59b99adbf6941a01ea9fbc
sqrtthree <imsqrtthree@gmail.com>
2019-09-28 02:39:03 +0800

    feat: Show short request id in logs when the app is running in debug mode

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,8 +1,14 @@
+import config from 'config'
 import Koa from 'koa'
+import _ from 'lodash'
 import uuidv3 from 'uuid/v3'
 import uuidv4 from 'uuid/v4'
 
 import loggerGenerator from '../utilities/logger'
+
+const debug = config.has('debug')
+  ? config.get('debug')
+  : process.env.FRIDAY_ENV === 'development'
 
 export default function(ctx: Koa.Context, next: Function): Promise<void> {
   const requestID =
@@ -10,7 +16,13 @@ export default function(ctx: Koa.Context, next: Function): Promise<void> {
     ctx.state.requestID ||
     uuidv3(Date.now().toString(), uuidv4())
 
-  ctx.logger = loggerGenerator(`request[${requestID}]`)
+  if (debug) {
+    const shortRequestID = _.first(_.split(requestID, '-'))
+
+    ctx.logger = loggerGenerator(`request[${shortRequestID}]`)
+  } else {
+    ctx.logger = loggerGenerator(`request`)
+  }
 
   return next()
 }
