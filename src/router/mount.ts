82c9d7faeb84a5e627e791fe2d7252f070b2bb58
sqrtthree <imsqrtthree@gmail.com>
2019-10-28 22:34:08 +0800

    refactor: Check if the user has exported a function when loading the user routes

--- src/router/mount.ts
+++ src/router/mount.ts
@@ -4,6 +4,7 @@ import loader from '../utilities/loader'
 import loggerGenerator from '../utilities/logger'
 
 const logger = loggerGenerator('friday:router')
+const noop = (): void => {}
 
 export default function mount(): Function {
   const routerPath = path.resolve(process.cwd(), 'dist/router.js')
@@ -20,7 +21,13 @@ export default function mount(): Function {
     logger.warn('Routes of your app is missed.')
 
     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    userRouter = function noop(): void {}
+    userRouter = noop
+  }
+
+  if (typeof userRouter !== 'function') {
+    throw new Error(
+      `Failed to load your routes from ${routerPath}. Expect function but got ${typeof userRouter}.`
+    )
   }
 
   return userRouter
