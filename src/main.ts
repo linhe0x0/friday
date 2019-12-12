6e3a595db3877426ce2e5b38a6b03e8cd4ee88ff
sqrtthree <imsqrtthree@gmail.com>
2019-12-12 22:13:18 +0800

    feat: expose config helpers

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ export { default as useLogger } from './utilities/logger'
 // App must be exported after useLogger due to side effect of router register.
 export { default as app } from './app'
 
+// Server
 export { default as getServer } from './utilities/get-server'
 
 /**
@@ -10,3 +11,12 @@ export { default as getServer } from './utilities/get-server'
  */
 export { default as validator } from './services/validator'
 export { validate } from './services/validator'
+
+/**
+ * Config
+ */
+export {
+  getConfig,
+  getOptionalConfig,
+  getConfigWithDefault,
+} from './services/config'
