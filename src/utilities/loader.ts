c8e2121779dba96345d13a81b58b0757b46251e7
sqrtthree <imsqrtthree@gmail.com>
2021-08-22 22:00:35 +0800

    refactor: small changes

--- src/utilities/loader.ts
+++ src/utilities/loader.ts
@@ -1,21 +1,21 @@
 import _ from 'lodash'
 
 // eslint-disable-next-line
-export default function loader(filepath: string) {
+export default function loader(filepath: string): any {
   let mod
 
   try {
     // eslint-disable-next-line
     mod = require(filepath)
 
-    const originalMod = mod
-
     // eslint-disable-next-line no-underscore-dangle
     if (mod && mod.__esModule && typeof mod === 'object' && mod.default) {
-      mod = mod.default // Await to support es6 module's default export
-    }
+      const originalMod = mod
 
-    mod = _.assign(mod, _.omit(originalMod, ['default']))
+      mod = mod.default // Await to support es6 module's default export
+
+      mod = _.assign(mod, _.omit(originalMod, ['default']))
+    }
   } catch (err) {
     throw new Error(`Failed to import ${filepath}: ${err.stack}`)
   }
