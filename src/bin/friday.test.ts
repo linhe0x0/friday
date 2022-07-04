5679597a82bc0fd51f3bfe89b2fd2281f0021f51
sqrtthree <imsqrtthree@gmail.com>
2022-07-04 22:42:56 +0800

    test: add test cases about subcommand calls

--- src/bin/friday.test.ts
+++ src/bin/friday.test.ts
@@ -44,6 +44,78 @@ describe('friday --help', () => {
   })
 })
 
+describe('run start command', () => {
+  beforeAll(() => {
+    jest.mock('./start')
+  })
+
+  afterAll(() => {
+    jest.unmock('./start')
+  })
+
+  test('should run start command when running without subcommand', async () => {
+    const start = jest.requireMock('./start')
+
+    const exit = jest
+      .spyOn<any, string>(process, 'exit')
+      .mockImplementation(() => {})
+
+    runCommand()
+
+    expect(start.default).toHaveBeenCalledTimes(1)
+
+    exit.mockRestore()
+  })
+
+  test('should run start command when running default command with specified entry-point', async () => {
+    const start = jest.requireMock('./start')
+
+    const exit = jest
+      .spyOn<any, string>(process, 'exit')
+      .mockImplementation(() => {})
+
+    runCommand('app.js')
+
+    expect(start.default).toHaveBeenCalledTimes(1)
+    expect(start.default.mock.calls[0][0]).toMatchObject({
+      entryPoint: 'app.js',
+    })
+
+    exit.mockRestore()
+  })
+
+  test('should run start command when running start subcommand', async () => {
+    const start = jest.requireMock('./start')
+
+    const exit = jest
+      .spyOn<any, string>(process, 'exit')
+      .mockImplementation(() => {})
+
+    runCommand('start')
+
+    expect(start.default).toBeCalledTimes(1)
+
+    exit.mockRestore()
+  })
+
+  test('should run start command when running start subcommand with specified entry-point', async () => {
+    const start = jest.requireMock('./start')
+
+    const exit = jest
+      .spyOn<any, string>(process, 'exit')
+      .mockImplementation(() => {})
+
+    runCommand('start', 'app.js')
+
+    expect(start.default).toBeCalledTimes(1)
+    expect(start.default.mock.calls[0][0]).toMatchObject({
+      entryPoint: 'app.js',
+    })
+
+    exit.mockRestore()
+  })
+})
+
 // Use the following to make the file a module, because the file cannot be
 // complied under '--isolatedModules' flag without it.
 // eslint-disable-next-line jest/no-export
