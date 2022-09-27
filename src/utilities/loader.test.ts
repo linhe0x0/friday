e6560c8aa4aa687afaac41225706b31526dc5d89
sqrtthree <imsqrtthree@gmail.com>
2022-09-27 20:21:17 +0800

    chore: pass the loader related test case

--- src/utilities/loader.test.ts
+++ src/utilities/loader.test.ts
@@ -3,18 +3,27 @@ import path from 'path'
 import loader from './loader'
 
 test('load json file', () => {
-  const pkg = loader(path.resolve(__dirname, '../../package.json'))
+  const pkg = loader(path.resolve(__dirname, './fixtures/loader/loader.json'))
 
-  expect(pkg.name).toBe('@sqrtthree/friday')
+  expect(pkg.name).toBe('loader json')
 })
 
 test('load common js file', () => {
-  const js = loader(path.resolve(__dirname, '../../dist/utilities/pkg.js'))
+  const js = loader(
+    path.resolve(__dirname, './fixtures/loader/loader-common.js')
+  )
 
   expect(typeof js).toBe('object')
   expect(js.default).toBeDefined()
-  expect(js.pkgInfo).toBeDefined()
-  expect(js.default.pkgInfo).toBe(js.pkgInfo)
+  expect(js.main).toBeDefined()
+  expect(js.default.main).toBe(js.main)
+
+  const js2 = loader(
+    path.resolve(__dirname, './fixtures/loader/loader-common2.js')
+  )
+
+  expect(typeof js2).toBe('function')
+  expect(js2.default).toBeDefined()
 })
 
 test('load ESModule js file', () => {
